26800203bdd2b3d8e070263b2fe62e3fc3da6bb3
SelamDawit <168993279+selamdawit@users.noreply.github.com>
2026-03-30 16:35:18 +0100

    Create MoMA artist database and clean data
    This SQL script creates a database and a table for storing artist data, loads data from a CSV file, and performs data cleaning operations such as trimming fields, handling empty values, standardizing gender values, and addressing missing year values.

---
+++ MoMa_Data_Cleaning.sql
@@ -0,0 +1,82 @@
+/*
+
+Cleaning Data in SQL Queries – MoMA Artists Table
+
+*/
+
+
+CREATE DATABASE moma_art;
+
+USE moma_art;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Create raw table to store imported artist data
+
+
+CREATE TABLE artists_raw (
+  ConstituentID INT PRIMARY KEY,
+  DisplayName TEXT,                
+  ArtistBio TEXT,                
+  Nationality TEXT,               
+  Gender TEXT,                    
+  BeginDate INT,                  
+  EndDate INT,                    
+  `Wiki QID` TEXT,                
+  ULAN TEXT                       
+);
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Load Data from CSV File
+
+
+LOAD DATA LOCAL INFILE '/Users/selam/Downloads/MoMA+Art+Collection/Artists.csv'
+INTO TABLE artists_raw
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Trim and Handle Empty Values in Fields
+
+
+UPDATE artists_raw
+SET
+  DisplayName = NULLIF(TRIM(DisplayName), ''),
+  ArtistBio = NULLIF(TRIM(ArtistBio), ''),
+  Nationality = NULLIF(TRIM(Nationality), ''),
+  Gender = NULLIF(TRIM(Gender), ''),
+  `Wiki QID` = NULLIF(TRIM(`Wiki QID`), ''),
+  ULAN = NULLIF(TRIM(ULAN), '');
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Standardize Gender Values
+
+
+UPDATE artists_raw
+SET Gender = LOWER(Gender)
+WHERE Gender IS NOT NULL;
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Handle Missing Year Values
+
+
+UPDATE artists_raw
+SET BeginDate = NULL
+WHERE BeginDate = 0;
+
+UPDATE artists_raw
+SET EndDate = NULL
+WHERE EndDate = 0;
+
+
+
+
+
+
